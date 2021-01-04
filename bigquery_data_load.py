f8ea320d1704717480680c2ea4c245ca38d759bd
Luis Felipe A. Venezian <felipelipe_11@hotmail.com>
2021-01-04 16:44:19 -0300

    bigquery data load dag

---
+++ bigquery_data_load.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+
+from airflow.utils.dates import days_ago
+
+default_arguments = {'owner': 'Luis', 'start_date': days_ago(1)}
+
+with DAG('bigquery_data_load', schedule_interval='@hourly', catchup=False, default_args= default_arguments) as dag:
+    load_data = GoogleCloudStorageToBigQueryOperator(
+        task_id = 'load_data',
+        bucket='lfav-bucket-stage',
+        source_objects=['*'],
+        source_format='CSV',
+        skip_leading_rows=1,
+        field_delimiter=',',
+        destination_project_dataset_table='lfav-apache-airflow-pipeline.lfavapacheairflowpipeline.history',
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_APPEND',
+        bigquery_conn_id='google_cloud_default',
+        google_cloud_storage_conn_id='google_cloud_default'
+    )
+
+    query = '''
+    SELECT * except(rank)
+    FROM (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY vehicle_id ORDER BY DATETIME(date, TIME(hour, minute, 0)) DESC
+            ) as rank
+            FROM `lfav-apache-airflow-pipeline.lfavapacheairflowpipeline.history`
+    ) AS latest
+    WHERE rank = 1;
+    '''
+
+    create_table = BigQueryOperator(
+        task_id='create_table',
+        sql=query,
+        destination_dataset_table='lfav-apache-airflow-pipeline.lfavapacheairflowpipeline.latest',
+        write_disposition='WRITE_TRUNCATE',
+        create_disposition='CREATE_IF_NEEDED',
+        use_legacy_sql=False,
+        location='southamerica-east1',
+        bigquery_conn_id='google_cloud_default'
+    )
+
+load_data >> create_table
